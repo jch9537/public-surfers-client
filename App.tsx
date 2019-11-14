e75a7659b63639e91fed818aa8f6e9be05bda6b0
jch9537 <jch9537@gmail.com>
2019-11-14 20:26:26 +0900

    🔧  SetUpDevEnvronment 1. eslint 삭제 2. tslint 추가

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Starting ChiMe Client!!!</Text>
-        </View>
-    );
+export interface AppProps {}
+
+export interface AppState {}
+
+class App extends React.Component<AppProps, AppState> {
+    // state = { :  };
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Starting ChiMe Client!!!</Text>
+            </View>
+        );
+    }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
     container: {

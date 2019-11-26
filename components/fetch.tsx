37c4741b580e650da0e2b09fb6ef43129532e1d1
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2019-11-26 16:01:45 +0900

    ✨ fetch 보내는 함수를 따로 만듬
    fetch를 보내는 부분을 따로빼서 파일로 만듬

--- components/fetch.tsx
+++ components/fetch.tsx
@@ -1,13 +1,64 @@
-const server = 'http://127.0.0.0:3000/user'; //서버주소
+let server = "http://server.url";
+export const user = (method: string, endPoint?: string, body?: object) => {
+    server += "/user";
+    if (method === "GET" || method === "DELETE" || endPoint === "signout") {
+        if (endPoint) {
+            server += "/" + endPoint
+        } else {
+            return new Error("잘 찾아봐라")
+        }
+        return fetch(server, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        })
+    } else {
+        if (endPoint) {
+            server += "/" + endPoint;
+        } else {
+            return new Error("endPoint를잘 찾아봐라")
+        }
+        return fetch(server, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body),
+            credentials: "include",
+        })
+    }
+}
+export const post = (method: string, body?: object, postId?: number, userId?: number) => {
+    server += "/post";
+    if (method === "GET") {
+        if (postId || userId) {
+            server += "/" + (userId || postId);
+        } else {
+            return new Error("잘 찾아봐라")
+        }
+        return fetch(server, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        })
+    } else {
+        if (postId && userId) {
+            server += `?user_id=${userId}$post_id=${postId}`
+        } else {
+            return new Error("잘 찾아봐라")
+        }
+        return fetch(server, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body),
+            credentials: "include",
+        })
+    }
 
-export function fetchAPI(url: string, method?: string, body?: object) {
-    return fetch(url, {
-        method: method,
-        mode: 'cors',
-        headers: {
-            'Contents-Type': 'application/json'
-        },
-        credentials: 'include',
-        body: JSON.stringify(body)
-    });
 }
